89796bfc18266f78e3215148b87fc78b1d9bfa8c
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-26 18:50:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 function volume_sphere() {
-    //Write your code here
+
+	let rele=document.getElementById('radius');
+	let vele=document.getElementById('volume');
+
+	let rvalue=rele.value;
+
+	
+
+	if(typeof(parseFloat(rvalue))==='number' && !isNaN(parseFloat(rvalue)) && parseFloat(rvalue)>=0)
+	{
+		let rva=parseFloat(rvalue);
+	let volume=(4/3)*3.14*(rva**3);
+		let vol=volume.toFixed(4);
+		vele.value=parseFloat(vol);
+	}
+	else
+	{
+	vele.value=NaN;	
+	}
+	
   
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+window.onload = document.getElementById('MyForm').onsubmit = function(e) {
+    e.preventDefault();
+    volume_sphere();
+};
